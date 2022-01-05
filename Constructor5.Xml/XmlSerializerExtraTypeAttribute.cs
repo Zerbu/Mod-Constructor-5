811683fccf618efe8cdece5497cab40d96d40b13
Zerbu <zerbu@LIVE.CO.UK>
2022-01-05 20:46:15 +0000

    Removed XML assembly

--- Constructor5.Xml/XmlSerializerExtraTypeAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Constructor5.Xml
-{
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
-    public class XmlSerializerExtraTypeAttribute : Attribute
-    {
-    }
-}
